03e7df74f3cecfc77afe9f6623568c10bd9a5b8b
zhiqiang <40115555+izhiqiang@users.noreply.github.com>
2025-04-15 13:08:40 +0800

    7.17.6

--- release.sh
+++ release.sh
@@ -2,8 +2,10 @@
 
 set -eux
 
+elasticsearch_version="7.17.6"
+
 docker login -u $DOCKER_USER -p $DOCKER_PWD
 
-docker build  -t zhiqiangwang/elasticsearch:5.6  .
+docker build  -t zhiqiangwang/elasticsearch:${elasticsearch_version} .
 
-docker push zhiqiangwang/elasticsearch:5.6
+docker push zhiqiangwang/elasticsearch:${elasticsearch_version} 
