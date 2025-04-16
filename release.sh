3124c656caedec95c586bc16d20b163a29cde562
zhiqiang <40115555+izhiqiang@users.noreply.github.com>
2025-04-16 13:44:13 +0800

    docker buildx

--- release.sh
+++ release.sh
@@ -6,6 +6,4 @@ elasticsearch_version="8.17.4"
 
 docker login -u $DOCKER_USER -p $DOCKER_PWD
 
-docker build  -t zhiqiangwang/elasticsearch:${elasticsearch_version} .
-
-docker push zhiqiangwang/elasticsearch:${elasticsearch_version} 
+docker buildx build --platform linux/amd64,linux/arm64 -t zhiqiangwang/elasticsearch:${elasticsearch_version}  --push .
